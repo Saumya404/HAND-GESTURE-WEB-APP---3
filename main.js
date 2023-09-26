8404684a17eddedadd5be466467b81297e39d166
Saumya404 <107306018+Saumya404@users.noreply.github.com>
2023-09-26 17:55:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+prediction_1="";
+prediction_2="";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    });
+}
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2Kf7WAwGP/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+
+}
+
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The first prediction is"+prediction_1;
+    speak_data_2="And the second prediction is"+prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+}
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+        if(results[0].label=="Thumbs Up"){
+            document.getElementById("update_emoji1").innerHTML="👍";
+        }
+        if(results[0].label=="Victory Hand"){
+            document.getElementById("update_emoji1").innerHTML="✌️";
+        } 
+        if(results[0].label=="Sign of the Horns"){
+            document.getElementById("update_emoji1").innerHTML="🤘";
+        }
+        if(results[0].label=="Hand with Fingers Splayed"){
+            document.getElementById("update_emoji1").innerHTML="🖐️";
+        }
+        if(results[0].label=="Call Me Hand"){
+            document.getElementById("update_emoji1").innerHTML="🤙";
+        }
+        if(results[0].label=="Crossed Fingers"){
+            document.getElementById("update_emoji1").innerHTML="🤞";
+        }
+
+        if(results[1].label=="Thumbs Up"){
+            document.getElementById("update_emoji2").innerHTML="👍";
+        }
+        if(results[1].label=="Victory Hand"){
+            document.getElementById("update_emoji2").innerHTML="✌️";
+        } 
+        if(results[1].label=="Sign of the Horns"){
+            document.getElementById("update_emoji2").innerHTML="🤘";
+        }
+        if(results[1].label=="Hand with Fingers Splayed"){
+            document.getElementById("update_emoji2").innerHTML="🖐️";
+        }
+        if(results[1].label=="Call Me Hand"){
+            document.getElementById("update_emoji2").innerHTML="🤙";
+        }
+        if(results[1].label=="Crossed Fingers"){
+            document.getElementById("update_emoji2").innerHTML="🤞";
+        }
+    }
+
+}
